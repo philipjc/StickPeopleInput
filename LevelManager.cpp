1089213c5bed9bab107afabad95149285ec64d88
Philip J Cox <philipjc@users.noreply.github.com>
2023-10-29 15:30:39 +0000

    Update LevelManager.cpp

--- LevelManager.cpp
+++ LevelManager.cpp
@@ -7,7 +7,7 @@
 using namespace sf;
 using namespace std;
 
-int** LevelManager::NextLevel(VertexArray& r_va_level)
+int** LevelManager::NextLevel(VertexArray& rVaLevel)
 {
 	m_LevelSize.x = 0;
 	m_LevelSize.y = 0;
@@ -55,11 +55,11 @@ int** LevelManager::NextLevel(VertexArray& r_va_level)
 	default: ;
 	}
 
-	ifstream input_file(levelToLoad);
+	ifstream inputFile(levelToLoad);
 	string s;
 
 	// Count the number of rows in the file
-	while (getline(input_file, s))
+	while (getline(inputFile, s))
 	{
 		++m_LevelSize.y;
 	}
@@ -68,8 +68,8 @@ int** LevelManager::NextLevel(VertexArray& r_va_level)
 	m_LevelSize.x = static_cast<int>(s.length());
 
 	// Go back to the start of the file
-	input_file.clear();
-	input_file.seekg(0, ios::beg);
+	inputFile.clear();
+	inputFile.seekg(0, ios::beg);
 
 	// Prepare the 2d array to hold the int values from the file
 	const auto arrayLevel = new int* [m_LevelSize.y];
@@ -82,7 +82,7 @@ int** LevelManager::NextLevel(VertexArray& r_va_level)
 	// Loop through the file and store all the values in the 2d array
 	string row;
 	int y = 0;
-	while (input_file >> row)
+	while (inputFile >> row)
 	{
 		for (int x = 0; x < row.length(); x++) {
 
@@ -94,13 +94,13 @@ int** LevelManager::NextLevel(VertexArray& r_va_level)
 	}
 
 	// close the file
-	input_file.close();
+	inputFile.close();
 
 	// What type of primitive are we using?
-	r_va_level.setPrimitiveType(Quads);
+	rVaLevel.setPrimitiveType(Quads);
 
 	// Set the size of the vertex array
-	r_va_level.resize(m_LevelSize.x * m_LevelSize.y * verts_in_quad);
+	rVaLevel.resize(m_LevelSize.x * m_LevelSize.y * verts_in_quad);
 
 	// Start at the beginning of the vertex array
 	int currentVertex = 0;
@@ -110,31 +110,31 @@ int** LevelManager::NextLevel(VertexArray& r_va_level)
 		for (int y = 0; y < m_LevelSize.y; y++)
 		{
 			// Position each vertex in the current quad
-			r_va_level[currentVertex + 0].position =
+			rVaLevel[currentVertex + 0].position =
 				Vector2f(static_cast<float>(x * tile_size), static_cast<float>(y * tile_size));
 
-			r_va_level[currentVertex + 1].position =
+			rVaLevel[currentVertex + 1].position =
 				Vector2f(static_cast<float>(x * tile_size + tile_size), static_cast<float>(y * tile_size));
 
-			r_va_level[currentVertex + 2].position =
+			rVaLevel[currentVertex + 2].position =
 				Vector2f(static_cast<float>(x * tile_size + tile_size), static_cast<float>(y * tile_size + tile_size));
 
-			r_va_level[currentVertex + 3].position =
+			rVaLevel[currentVertex + 3].position =
 				Vector2f(static_cast<float>(x * tile_size), static_cast<float>(y * tile_size + tile_size));
 
 			// Which tile from the sprite sheet should we use
 			const int verticalOffset = arrayLevel[y][x] * tile_size;
 
-			r_va_level[currentVertex + 0].texCoords =
+			rVaLevel[currentVertex + 0].texCoords =
 				Vector2f(0, static_cast<float>(0 + verticalOffset));
 
-			r_va_level[currentVertex + 1].texCoords =
+			rVaLevel[currentVertex + 1].texCoords =
 				Vector2f(static_cast<float>(tile_size), static_cast<float>(0 + verticalOffset));
 
-			r_va_level[currentVertex + 2].texCoords =
+			rVaLevel[currentVertex + 2].texCoords =
 				Vector2f(static_cast<float>(tile_size), static_cast<float>(tile_size + verticalOffset));
 
-			r_va_level[currentVertex + 3].texCoords =
+			rVaLevel[currentVertex + 3].texCoords =
 				Vector2f(0, static_cast<float>(tile_size + verticalOffset));
 
 			// Position ready for the next four vertices
